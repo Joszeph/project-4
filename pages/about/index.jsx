02261c7463da1125d36999a9fb85c36614922788
Yosif Georgiev <iodesign@live.com>
2023-02-07 21:26:07 +0200

    NEW: Task 5 - User Component

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -4,13 +4,14 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Link from "../../src/components/link/Link";
 import Header from "../../src/components/header/Header";
-import Avatar from "../../src/components/avatar/Avatar";
+import User from "../../src/components/user/User";
+
 
 export default function About() {
   return (
     <>
       <Header />
-      <Avatar />
+<User />
       <Container maxWidth="sm">
         <Grid container justifyContent="center">
           <Grid item>
---
+++ src/components/user/User.jsx
@@ -0,0 +1,21 @@
+import Avatar from "../avatar/Avatar";
+import classNames from "classnames";
+import styles from "./User.module.scss";
+
+export default function User({
+  name = "terika77",
+  info = "134 Items",
+  avatar = "",
+  size = 55,
+  verified = false,
+}) {
+  return (
+    <div className={classNames(styles.user)}>
+      <Avatar />
+      <div className={classNames(styles.userInfo)}>
+        <p className={classNames(styles.name)}>{name}</p>
+        <p className={classNames(styles.info)}>{info}</p>
+      </div>
+    </div>
+  );
+}
