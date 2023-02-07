8e91197cb58c98a730ead5e609c9fe6ab54d9e87
Yosif Georgiev <iodesign@live.com>
2023-02-07 19:48:21 +0200

    NEW: Task 3 - Header Component

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -3,20 +3,21 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Link from "../../src/components/link/Link";
-import Logo from "../../src/components/logo/Logo";
-
+import Header from "../../src/components/header/Header";
 
 export default function About() {
   return (
-    <Container maxWidth="sm">
-      <Grid container justifyContent="center">
-        <Grid item>
-          <Button variant="contained" component={Link} noLinkStyle href="/">
-            🏠 Home
-          </Button>
-          <Logo type="default"/>
+    <>
+      <Header />
+      <Container maxWidth="sm">
+        <Grid container justifyContent="center">
+          <Grid item>
+            <Button variant="contained" component={Link} noLinkStyle href="/">
+              🏠 Home
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 }
