a2c99c378848b3c301de2998be72fce2eee5e5c6
Yosif Georgiev <iodesign@live.com>
2023-02-09 14:51:21 +0200

    FIX: Task 10 - Footer Component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,13 +10,13 @@ export default function Copyright() {
     <footer className={classNames(styles.footerWrapper)}>
       <Container className={classNames(styles.container)} maxWidth="lg">
         <Grid container alignItems="center" justifyContent="space-around" spacing={10}>
-          <Grid item >
+          <Grid item xs={4}>
             <Logo type="muted" />
           </Grid>
-          <Grid item >
+          <Grid item xs={4}>
             <p>Bum All Rights Reserved 2021</p>
           </Grid>
-          <Grid item >
+          <Grid item xs={4}>
             <Button className={classNames(styles.footerBtn)}>Privacy Policy</Button>
             <Button className={classNames(styles.footerBtn)}>Cookie Policy</Button>
           </Grid>
