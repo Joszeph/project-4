56dc90eb5ab52c70db3bf258df154935544c49ec
Yosif Georgiev <iodesign@live.com>
2023-02-09 14:29:40 +0200

    FIX: Task 10 - Footer Component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,4 @@
-import Container from "@mui/material/Container";
-import {Grid} from "@mui/material";
-import {Button} from "@mui/material";
+import { Grid, Container, Button } from "@mui/material";
 
 import styles from "./Footer.module.scss";
 import classNames from "classnames";
@@ -12,13 +10,13 @@ export default function Copyright() {
     <footer className={classNames(styles.footerWrapper)}>
       <Container className={classNames(styles.container)} maxWidth="lg">
         <Grid container alignItems="center" justifyContent="space-around" spacing={10}>
-          <Grid item>
+          <Grid item >
             <Logo type="muted" />
           </Grid>
-          <Grid item>
+          <Grid item >
             <p>Bum All Rights Reserved 2021</p>
           </Grid>
-          <Grid item>
+          <Grid item >
             <Button className={classNames(styles.footerBtn)}>Privacy Policy</Button>
             <Button className={classNames(styles.footerBtn)}>Cookie Policy</Button>
           </Grid>
