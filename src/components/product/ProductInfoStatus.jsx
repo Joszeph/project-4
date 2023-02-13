100dad09a60184d72632e628521414bb755e4fe1
Yosif Georgiev <iodesign@live.com>
2023-02-13 18:52:39 +0200

    FIX: Task 19 - Product Info Status

--- src/components/product/ProductInfoStatus.jsx
+++ src/components/product/ProductInfoStatus.jsx
@@ -1,14 +1,19 @@
-import * as React from 'react';
-import { Chip } from '@mui/material'
-import { Circle } from '@mui/icons-material'
-
-import styles from './ProductInfoStatus.module.scss'
+import * as React from "react";
 import classNames from "classnames";
+import styles from "./ProductInfoStatus.module.scss";
+import { Chip } from "@mui/material";
+import Circle from "@mui/icons-material/Circle";
 
 export default function ProductInfoStatus() {
   return (
     <div className={classNames(styles["product-info-status"])}>
-         <Chip className={classNames(styles["status"])} color="secondary" variant="filled" icon = {<Circle></Circle>} label = {"LIVE"}></Chip>
+      <Chip
+        color="secondary"
+        className={classNames(styles.status)}
+        variant="filled"
+        icon={<Circle></Circle>}
+        label={"LIVE"}
+      ></Chip>
     </div>
-  )
+  );
 }
