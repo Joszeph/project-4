9257d35e42c97005eaf3616fb2f0a011d21080ca
Yosif Georgiev <iodesign@live.com>
2023-02-13 17:14:21 +0200

    FIX: Task 17 - Product Title Info

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import styles from "./ProductInfoTitle.scss";
+import * as React from 'react';
+import styles from './ProductInfoTitle.module.scss';
+import { Typography } from '@mui/material';
 import classNames from "classnames";
-import {Typography} from "@mui/material";
 
-export default function ProductInfoTitle({ text }) {
-  return (
-    <div className={classNames(styles["product-info-title"])}>
-      <Typography variant="h1" className={classNames(styles["title"])}>
-        {text}
-      </Typography>
-    </div>
-  );
+export default function ProductInfoTitle({text}) {
+    return (
+        <div className={classNames(styles['product-info-title'])}>
+            <Typography variant="h1" className={classNames(styles['title'])}>
+                {text}
+            </Typography>
+        </div>
+    )
 }
