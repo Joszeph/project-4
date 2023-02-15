2ce9df1f20ccc427a019f0c4ae3ab2012db0ef9c
Yosif Georgiev <iodesign@live.com>
2023-02-15 17:27:25 +0200

    FIX: Task 26 - Product Container

--- src/components/product/ProductContainer.jsx
+++ src/components/product/ProductContainer.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import classNames from "classnames";
-import styles from "./ProductContainer.module.scss";
+import styles from "./ProductContainer";
 
 import { Container, Grid, Box, Item } from "@mui/material";
 
