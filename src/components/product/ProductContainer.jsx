022cefaa24ac520be194cbecc635620f2fddfb7d
Yosif Georgiev <iodesign@live.com>
2023-02-15 18:27:16 +0200

    UPDATE: Task 26 - Product Container

--- src/components/product/ProductContainer.jsx
+++ src/components/product/ProductContainer.jsx
@@ -1,23 +1,26 @@
-import styles from './ProductContainer.module.scss';
-import { Grid, Box, Container} from '@mui/material';
+import styles from "./ProductContainer.module.scss";
+import { Grid, Box, Container } from "@mui/material";
 import classNames from "classnames";
+import React, { useEffect } from "react";
+import ProductImage from "./ProductImage";
+import ProductActions from "./ProductActions";
+import ProductInfo from "./ProductInfo";
+import ProductTabs from "./ProductTabs";
 
-import ProductImage from './ProductImage';
-import ProductActions from './ProductActions';
-import ProductInfo from './ProductInfo';
-import ProductTabs from './ProductTabs';
+export default function ProductContainer({ product }) {
 
 
-export default function ProductContainer({product}) {
-
-    return (
-        <Container maxWidth={false} className={classNames(styles["product-container"])}>
-            <Grid container spacing={6}>
-                <Grid item xs={6}>
-               <ProductImage url={product?.source?.url}/>
-                </Grid>
-                <Grid item xs={5}>
-                <ProductInfo
+  return (
+    <Container
+      maxWidth={false}
+      className={classNames(styles["product-container"])}
+    >
+      <Grid container spacing={6}>
+        <Grid item xs={6}>
+          <ProductImage url={product?.source?.url} />
+        </Grid>
+        <Grid item xs={5}>
+          <ProductInfo
             title={product?.name}
             creator={product?.owner}
             price={product?.price}
@@ -26,7 +29,7 @@ export default function ProductContainer({product}) {
             timeEnd={product?.auction_end}
             isLive={product?.auction_end}
             onTimeEnd={product?.auction_end}
-          /> 
+          />
           <ProductTabs bids={product?.bids} text={product?.details} />
           <ProductActions
             isLive={product?.auction_end}
@@ -36,8 +39,8 @@ export default function ProductContainer({product}) {
             onBid={product?.bids}
             onBuy={product?.bids}
           />
-               </Grid>
-            </Grid>
-        </Container>
-    )
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
