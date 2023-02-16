010868ee93865317851e405907238ecb9054be76
Yosif Georgiev <iodesign@live.com>
2023-02-16 13:39:18 +0200

    NEW: Task 28 - Hero Component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+import classNames from "classnames";
+import styles from "./Hero.module.scss";
+
+function Hero({text=''}) {
+  return (
+    <div className={classNames(styles.hero)}>
+        <Typography className={classNames(styles.text)} variant="h1">
+            {text}
+        </Typography>
+    </div>
+  )
+}
+
+export default Hero
