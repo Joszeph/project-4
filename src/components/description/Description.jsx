bcb413551ed02345842e81b1f174bcbe247cf722
Yosif Georgiev <iodesign@live.com>
2023-02-16 14:28:34 +0200

    NEW: Task 30 - How Page

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 import classNames from "classnames";
 import styles from "./Description.module.scss";
 
-export default function Description({text="Lorem askjfkasdfgi", image}) {
+export default function Description({text, image}) {
   return (
     <div className={classNames(styles.description)}>
         <Typography className={classNames(styles.text)} variant="body1">
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 import classNames from "classnames";
 import styles from "./Hero.module.scss";
 
-export default function Hero({text="title"}) {
+export default function Hero({text}) {
   return (
     <div className={classNames(styles.hero)}>
         <Typography className={classNames(styles.text)} variant="h1">
