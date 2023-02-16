1f05c7bfbd34339ff4737eb01514395fc871fb2b
Yosif Georgiev <iodesign@live.com>
2023-02-16 14:39:13 +0200

    NEW: Task 31 - ProfileHero

---
+++ src/components/profileHero/ProfileHero.jsx
@@ -0,0 +1,11 @@
+import classNames from "classnames";
+import styles from "./ProfileHero.module.scss";
+
+export default function ProfileHero({image}) {
+  return (
+    <div className={classNames(styles["profile-hero"])}>
+        <img className={classNames(styles.image)} src={image} alt="" />
+    </div>
+  )
+}
+
