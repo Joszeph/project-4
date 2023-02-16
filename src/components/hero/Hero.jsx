03f2949e6c8f32ca0aae210a25117b71750913f2
Yosif Georgiev <iodesign@live.com>
2023-02-16 14:21:07 +0200

    UPDATE: Task 28 - Hero Component - SCSS

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 import classNames from "classnames";
 import styles from "./Hero.module.scss";
 
-export default function Hero({text}) {
+export default function Hero({text="title"}) {
   return (
     <div className={classNames(styles.hero)}>
         <Typography className={classNames(styles.text)} variant="h1">
