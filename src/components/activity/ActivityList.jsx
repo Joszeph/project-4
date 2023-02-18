eb1e88cf8e7844d39945a91e43836cb6c375605c
Yosif Georgiev <iodesign@live.com>
2023-02-18 20:07:08 +0200

    FIX: Task 38 - Activity List Component

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -3,29 +3,29 @@ import React from "react";
 import classNames from "classnames";
 import styles from "./ActivityList.module.scss";
 
-import ActivityListItems from "./ActivityListItem";
+import ActivityListItem from "./ActivityListItem";
 import { Stack } from "@mui/material";
-
-
 
 export default function ActivityList({ items = [] }) {
   return (
     <div className={classNames(styles["activity-list"])}>
       <Stack
         direction="column"
+        spacing={2}
         justifyContent="flex-start"
         alignItems="center"
-        spacing={2}
         className={classNames(styles["activity-item"])}
       >
-        {items.map((item) => {
-          <ActivityListItems
-            key={index}
-            user={item.user}
-            created_at={item.created_at}
-            nft={item.nft}
-            type={item.type}
-          />
+        {items.map((item, index) => {
+          return (
+            <ActivityListItem
+              key={index}
+              user={item.user}
+              created_at={item.created_at}
+              nft={item.nft}
+              type={item.type}
+            />
+          );
         })}
       </Stack>
     </div>
