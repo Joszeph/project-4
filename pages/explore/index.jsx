0a06f484d2b931df36243b0105466c18730946fa
Yosif Georgiev <iodesign@live.com>
2023-02-21 20:19:31 +0200

    FIX: Task 42 - Explore Page

--- pages/explore/index.jsx
+++ pages/explore/index.jsx
@@ -1,87 +1,47 @@
-import React from "react";
-
-import Header from "../../src/components/header/Header";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import ExploreTitle from "../../src/components/explore/ExploreTitle";
 import ExploreFilters from "../../src/components/explore/ExploreFilters";
+import Header from "../../src/components/header/Header";
 import Footer from "../../src/components/footer/Footer";
+import Card from "../../src/components/card/Card";
 
-import  Container  from "@mui/material/Container";
-import Card from "@mui/material/Card";
-import Grid from "@mui/material/Grid";
-
-import nftsDb from "../../data/nfts.json";
+import nfts from '../../data/nfts.json'
 
 export default function Explore() {
-  const [nfts, setNfts] = useState([]);
-  const [nftFilters, setNftFilters] = useState([]);
-  const [sort, setSort] = useState();
-  const [price, setPrice] = useState();
 
-  const baseUrl = process.env.apiUrl;
-
-  useEffect(async () => {
-    try {
-      const result = await fetch(nftsDb);
-      const response = await result.json();
-
-      setNfts(response.nfts);
-      setNftFilters(response.filters);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  useEffect(async () => {
-    const result = await fetch(nftsDb);
-    const response = await result.json();
-    setNfts(response.nfts);
-  }, [sort]);
-
-  useEffect(async () => {
-    const result = await fetch(nftsDb);
-    const response = await result.json();
-    setNfts(response.nfts);
-  }, [price]);
-
-  const handlePriceRangeChange = (event) => {
-    setPrice(event.target.value);
-  };
-  const handleSortByChange = (event) => {
-    setSort(event.target.value);
-  };
 
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       <Header />
-      <Container maxWidth="xl" sx={{ margin: "10px 0" }}>
-        <Grid container>
-          <Grid item xs={5}>
-            <ExploreTitle />
+      <Container maxWidth="xl">
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          spacing={3}
+          alignItems="flex-start"
+        >
+          <Grid item xs="3">
+            <ExploreTitle text="Explore" />
           </Grid>
-          <Grid item xs={7}>
-            <ExploreFilters
-              filters={nftFilters}
-              handlePriceRangeChange={handlePriceRangeChange}
-              handleSortByChange={handleSortByChange}
-            />
+          <Grid item xs="7">
+            <ExploreFilters  />
           </Grid>
         </Grid>
-        <Grid container spacing={1}>
-          {nfts.map((n, index) => {
-            return (
-              <Grid item xs={3} key={index}>
-                <Card
-                  name={n.name}
-                  likes={n.likes}
-                  mediaUrl={n.mediaUrl}
-                  user={n.owner}
-                  price={n.price}
-                  currency={n.currency}
-                  timeLeft={n.timeLeft}
-                />
-              </Grid>
-            );
-          })}
+        <Grid container spacing={3} maxWidth="lg" sx={{ marginLeft: 3 }}>
+          {nfts.map((nft) => (
+            <Grid item xs="3">
+              <Card
+                name={nft.name}
+                mediaUrl={nft.source.url}
+                user={nft.owner}
+                price={nft.price}
+                currency={nft.currency}
+                likes={nft.likes}
+              ></Card>
+            </Grid>
+          ))}
         </Grid>
       </Container>
       <Footer />
