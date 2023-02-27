f8f445d1b006ccf92ec96f3f6c60422fbe70c03d
Yosif Georgiev <iodesign@live.com>
2023-02-27 15:04:35 +0200

    FIX: Task 51 - Profile page Api Calls

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,37 +1,36 @@
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/dist/client/router'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/dist/client/router";
 
-import Header from '../../src/components/header/Header'
-import ProfileHero from '../../src/components/profile/ProfileHero'
-import ProfileUser from '../../src/components/profile/ProfileUser'
-import ProfileCollection from '../../src/components/profile/ProfileCollection'
-import Footer from '../../src/components/footer/Footer'
+import Header from "../../src/components/header/Header";
+import ProfileHero from "../../src/components/profile/ProfileHero";
+import ProfileUser from "../../src/components/profile/ProfileUser";
+import ProfileCollection from "../../src/components/profile/ProfileCollection";
+import Footer from "../../src/components/footer/Footer";
 
 // import profile from '../../data/profile.json'
 
-export default function Profile({profile}) {
+export default function Profile({ profile }) {
+  const router = useRouter();
+  const { id } = router.query;
+  const url = process.env.apiUrl;
 
-const router = useRouter()  
-const {id} = router.query
-const url= process.env.apiUrl
+  const [profile, setProfile] = useState([]);
+  const [profileFilters, setProfileFilters] = useState([]);
 
-const [user, setUser] = useState([])
-const [filters, setFilters] = useState([])
-
-useEffect(async()=>{
-  const response = await fetch(`${url}/users/${id}`)
-  const result = await response.json()
-  setUser(result)
-  setFilters(result.filters)
-})
+  useEffect(async () => {
+    const result = await fetch(`${url}/users/${id}`);
+    const response = await result.json();
+    setProfile(response.profile);
+    setProfileFilters(response.filters);
+  });
 
   return (
     <div>
-        <Header />
-        <ProfileHero />
-        <ProfileUser />
-        <ProfileCollection user={user} filters={filters}/>
-        <Footer />
+      <Header />
+      <ProfileHero />
+      <ProfileUser />
+      <ProfileCollection user={profile} filters={profileFilters} />
+      <Footer />
     </div>
-  )
+  );
 }
