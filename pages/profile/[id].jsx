fa7d6954bb7e544644853dd7a0dd90391a161156
Yosif Georgiev <iodesign@live.com>
2023-02-27 14:48:35 +0200

    NEW: Task 51 - Profile page Api Calls

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,18 +1,33 @@
+import { useState, useEffect } from 'react'
+
 import Header from '../../src/components/header/Header'
 import ProfileHero from '../../src/components/profile/ProfileHero'
 import ProfileUser from '../../src/components/profile/ProfileUser'
 import ProfileCollection from '../../src/components/profile/ProfileCollection'
 import Footer from '../../src/components/footer/Footer'
 
-import profile from '../../data/profile.json'
+// import profile from '../../data/profile.json'
 
 export default function Profile({profile}) {
+
+const url= process.env.apiUrl
+
+const [user, setUser] = useState([])
+const [filters, setFilters] = useState([])
+
+useEffect(async()=>{
+  const response = await fetch(`${url}/users/${id}`)
+  const result = await response.json()
+  setUser(result)
+  setFilters(result.filters)
+})
+
   return (
     <div>
         <Header />
         <ProfileHero />
         <ProfileUser />
-        <ProfileCollection />
+        <ProfileCollection user={user} filters={filters}/>
         <Footer />
     </div>
   )
