556c242af809e378babeee116718ceceda10a878
Yosif Georgiev <iodesign@live.com>
2023-03-06 21:45:28 +0200

    FIX: Task-53 Add requests by filters in Activity Page component

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -1,81 +1,116 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
+// import Header from "../../src/components/header/Header";
+// import Hero from "../../src/components/hero/Hero.jsx";
+// import ActivityFilters from "../../src/components/activity/ActivityFilters";
+// import ActivityList from "../../src/components/activity/ActivityList";
+// import Footer from "../../src/components/footer/Footer";
+
+// // import activityDb from '../../data/activity.json'
+
+// export default function index() {
+//   const url = process.env.apiUrl;
+
+//   const [activity, setActivity] = useState([]);
+//   const [activityFilters, setActivityFilters] = useState([]);
+//   const [sortBy, setSortBy] = useState("");
+//   const [price, setType] = useState("");
+
+//   useEffect(async () => {
+//     try {
+//       const response = await fetch(`${url}/activities`);
+//       const result = await response.json();
+//       setActivity(result.activities);
+//       setActivityFilters(result.filters);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }, []);
+
+//   const buildApiUrl = () => {
+//     let url = `${process.env.apiUrl}/activities`;
+
+//     if (sortBy) {
+//       url += `?sort=${sortBy}`;
+//     }
+
+//     if (price) {
+//       url += `${sortBy ? '&' : '?'}price=${price}`;
+//     }
+
+//     return url;
+//   };
+
+//   useEffect(async () => {
+//     try{
+//       const response = await fetch(buildApiUrl());
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+//       const result = await response.json();
+//       setActivity(result.activities);
+//       setActivityFilters(result.filters);
+//     }catch(error){
+//       console.log(error)
+//     }
+
+//   }, [sortBy, price]);
+
+
+//   const handleSortChange = (e) => {
+//     setSortBy(e.target.value);
+//   };
+
+//   const handleTypeChange = (e) => {
+//     setType(e.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <Header />
+//       <Hero text="Activity" />
+//       <ActivityFilters
+//         filters={activityFilters}
+//         handleSortChange={handleSortChange}
+//         handleTypeChange={handleTypeChange}
+//       />
+//       <ActivityList items={activity} />
+//       <Footer />
+//     </div>
+//   );
+// }
+import * as React from "react";
 import Header from "../../src/components/header/Header";
-import Hero from "../../src/components/hero/Hero.jsx";
-import ActivityFilters from "../../src/components/activity/ActivityFilters";
-import ActivityList from "../../src/components/activity/ActivityList";
 import Footer from "../../src/components/footer/Footer";
+import Hero from "../../src/components/hero/Hero";
+import ActivityList from "../../src/components/activity/ActivityList";
+import ActivityFilters from "../../src/components/activity/ActivityFilters";
 
-// import activityDb from '../../data/activity.json'
-
-export default function index() {
-  const url = process.env.apiUrl;
-
-  const [activity, setActivity] = useState([]);
-  const [activityFilters, setActivityFilters] = useState([]);
-  const [sortBy, setSortBy] = useState("");
-  const [price, setType] = useState("");
-
-  useEffect(async () => {
-    try {
-      const response = await fetch(`${url}/activities`);
-      const result = await response.json();
-      setActivity(result.activities);
-      setActivityFilters(result.filters);
-    } catch (err) {
-      console.log(err);
+export default function index(){
+    const [activity, setActivity] = React.useState();
+    const [activityFilters, setActivityFilters] = React.useState();
+    async function fetchActivities(){
+      const fetchJson = async ()=>{
+        const response = await fetch(process.env.apiUrl+"/activities");
+        const results = await response.json();  
+        return results;
+        };
+      const jsons = await  fetchJson();
+      
+      setActivity(jsons.activities);
+      setActivityFilters(jsons.filters);
     }
-  }, []);
+    React.useEffect(() => {
+        fetchActivities();
+    }, []);
 
-  const buildApiUrl = () => {
-    let url = `${process.env.apiUrl}/activities`;
-
-    if (sortBy) {
-      url += `?sort=${sortBy}`;
-    }
-
-    if (price) {
-      url += `${sortBy ? '&' : '?'}price=${price}`;
-    }
-
-    return url;
-  };
-
-  useEffect(async () => {
-    try{
-      const response = await fetch(buildApiUrl());
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const result = await response.json();
-      setActivity(result.activities);
-      setActivityFilters(result.filters);
-    }catch(error){
-      console.log(error)
-    }
-
-  }, [sortBy, price]);
-
-
-  const handleSortChange = (e) => {
-    setSortBy(e.target.value);
-  };
-
-  const handleTypeChange = (e) => {
-    setType(e.target.value);
-  };
-
-  return (
-    <div>
-      <Header />
-      <Hero text="Activity" />
-      <ActivityFilters
-        filters={activityFilters}
-        handleSortChange={handleSortChange}
-        handleTypeChange={handleTypeChange}
-      />
-      <ActivityList items={activity} />
-      <Footer />
-    </div>
-  );
+    return (
+        <div style={{width:"100%"}}>   
+            <Header></Header>
+            <Hero text={"Activity"}></Hero>
+             <ActivityFilters filters={activityFilters}></ActivityFilters>
+            <ActivityList items={activity}></ActivityList>
+            <Footer></Footer>
+        </div>
+        );
 }
--- src/components/activity/ActivityFilters.jsx
+++ src/components/activity/ActivityFilters.jsx
@@ -1,88 +1,144 @@
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+// import { useState } from "react";
 
+// import classNames from "classnames";
+// import styles from "./ActivityFilters.module.scss";
+// import {
+//   FormControl,
+//   Select,
+//   InputLabel,
+//   MenuItem,
+//   Stack,
+//   TextField,
+//   InputAdornment,
+//   Search,
+// } from "@mui/material";
+// import SearchIcon from "@mui/icons-material/Search";
+
+// export default function ActivityFilters({
+//   filters = [],
+//   handleSortChange,
+//   handleTypeChange,
+// }) {
+
+//   const [sortBy, setSortBy] = useState("");
+//   const [price, setType] = useState("");
+
+//   return (
+//     <div className={classNames(styles["activity-filters"])}>
+//       <Stack direction="row" spacing={2} alignItems="center">
+//         <FormControl
+//           className={classNames(styles.form)}
+//           variant="filled"
+//           margin="dense"
+//           size="small"
+//           sx={{ minWidth: 150 }}
+//         >
+//           <InputLabel id="sort-by"></InputLabel>
+//           <Select
+//             variant="outlined"
+//             value={sortBy}
+//             className={classNames(styles.select)}
+//             onChange={(e) => {
+//               handleSortChange(e);
+//               setSortBy(e.target.value);
+//             }}
+//           >
+//             {Array.isArray(filters.sort) &&
+//               filters.sort.map((element) => (
+//                 <MenuItem value={element.value} filter={element.label} key={element.value}/>
+//               ))}
+//           </Select>
+//         </FormControl>
+//         <FormControl
+//           variant="filled"
+//           margin="dense"
+//           size="small"
+//           sx={{ minWidth: 150 }}
+//         >
+//           <InputLabel id="type"></InputLabel>
+//           <Select
+//             variant="outlined"
+//             value={price}
+//             className={classNames(styles.select)}
+//             onChange={(e) => {
+//               handleTypeChange(e);
+//               setType(e.target.value);
+//             }}
+//           >
+//             {Array.isArray(filters.type) &&
+//               filters.type.map((element) => (
+//                 <MenuItem value={element.value} filter={element.label} key={element.value}/>
+//               ))}
+//           </Select>
+//         </FormControl>
+//         <TextField
+//           variant="standard"
+//           InputProps={{
+//             startAdornment: (
+//               <InputAdornment position="start">
+//                 <SearchIcon className={classNames(styles.icon)} />
+//               </InputAdornment>
+//             ),
+//           }}
+//         />
+//       </Stack>
+//     </div>
+//   );
+// }
+import * as React from 'react';
 import classNames from "classnames";
 import styles from "./ActivityFilters.module.scss";
-import {
-  FormControl,
-  Select,
-  InputLabel,
-  MenuItem,
-  Stack,
-  TextField,
-  InputAdornment,
-  Search,
-} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import  Typography  from '@mui/material/Typography';
+import Grid from "@mui/material/Grid";
+import Input from "@mui/material/Input";
+import Search from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import Select from "@mui/material/Select";
+import MenuItem from '@mui/material/MenuItem';
+import CollectorColumn from "../collectors/CollectorColumn";
+import TextField from "@mui/material/TextField";
+import  InputLabel  from '@mui/material/InputLabel';
+import Stack from '@mui/material/Stack';
+import { PriceChange } from '@mui/icons-material';
+import FormControl from "@mui/material/FormControl";
 
-export default function ActivityFilters({
-  filters = [],
-  handleSortChange,
-  handleTypeChange,
-}) {
-
-  const [sortBy, setSortBy] = useState("");
-  const [price, setType] = useState("");
-
-  return (
-    <div className={classNames(styles["activity-filters"])}>
-      <Stack direction="row" spacing={2} alignItems="center">
-        <FormControl
-          className={classNames(styles.form)}
-          variant="filled"
-          margin="dense"
-          size="small"
-          sx={{ minWidth: 150 }}
-        >
-          <InputLabel id="sort-by"></InputLabel>
-          <Select
-            variant="outlined"
-            value={sortBy}
-            className={classNames(styles.select)}
-            onChange={(e) => {
-              handleSortChange(e);
-              setSortBy(e.target.value);
-            }}
-          >
-            {Array.isArray(filters.sort) &&
-              filters.sort.map((element) => (
-                <MenuItem value={element.value} filter={element.label} key={element.value}/>
-              ))}
+export default function ActivityFilters({filters={sort: [], type:[]}}) {
+    const [sortBy, setSort] = React.useState();
+    const [types, setType] = React.useState();
+    //console.log(filters.sort);
+   // console.log(filters.price);
+   const handleSortChange = (event)=>{
+        setSort(event.target.value);
+    };
+    const handleTypeChange = (event)=>{
+        setType(event.target.value);
+    };
+    return (
+      
+      <div className={classNames(styles["activity-filters"])}>
+        <Stack direction = "row" spacing = {2} alignItems="center">
+        <FormControl variant="filled"  margin="dense"  size="small"  sx={{minWidth: 150 }} >
+        <InputLabel   id="sort-select-standard-label">Sort by</InputLabel>
+          <Select   className={classNames(styles.select)} variant='outlined' value={sortBy} onChange = {handleSortChange} labelId="sort-select-standard-label" >
+            {filters.sort.map((element)=>(
+                <MenuItem value={element.value}> {element.label} </MenuItem> 
+                 ))
+            }
+            </Select>
+        </FormControl> 
+        <FormControl variant="filled"  size='small' sx={{minWidth: 150 }}>
+        <InputLabel    id="type-select-standard-label">Type</InputLabel>
+          <Select className={classNames(styles.select)}  variant='outlined' value={types} onChange = {handleTypeChange} labelId="type-select-standard-label" >
+          {filters.type.map((element)=>(
+                <MenuItem value={element.value}> {element.label} </MenuItem> 
+            ))
+            }
           </Select>
-        </FormControl>
-        <FormControl
-          variant="filled"
-          margin="dense"
-          size="small"
-          sx={{ minWidth: 150 }}
-        >
-          <InputLabel id="type"></InputLabel>
-          <Select
-            variant="outlined"
-            value={price}
-            className={classNames(styles.select)}
-            onChange={(e) => {
-              handleTypeChange(e);
-              setType(e.target.value);
-            }}
-          >
-            {Array.isArray(filters.type) &&
-              filters.type.map((element) => (
-                <MenuItem value={element.value} filter={element.label} key={element.value}/>
-              ))}
-          </Select>
-        </FormControl>
-        <TextField
-          variant="standard"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon className={classNames(styles.icon)} />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Stack>
-    </div>
-  );
-}
+        </FormControl>        
+          <TextField  className  = {classNames(styles.search)}  variant="standard" InputProps={{startAdornment: <InputAdornment position="start"><Search className  = {classNames(styles.icon)}/></InputAdornment>}}></TextField>
+          </Stack>
+      </div>
+      
+    );
+  }
